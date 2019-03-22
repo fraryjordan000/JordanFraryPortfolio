0145526c9bb25c05e7e6c67c1fd58cba9f36bf41
JordanMac <fraryjordan000@gmail.com>
2019-03-22 17:26:35 -0600

    Added basic components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { LiveComponent } from './live/live.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'certifications', component: CertificationsComponent},
+  {path: 'experience', component: ExperienceComponent},
+  {path: 'education', component: EducationComponent},
+  {path: 'live', component: LiveComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
